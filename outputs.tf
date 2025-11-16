b172063d349b82a6b57b3d15c6a54b3a2ed6e877
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-11-16 15:52:55 +0530

    provided public and private ips output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "availability_zone" {
   description = "The availability zone of the instance"
   value       = aws_instance.main.availability_zone
 }
+
+output "public_ip" {
+  description = "The EC2 instance Public IP"
+  value       = aws_instance.main.public_ip
+}
+
+output "private_ip" {
+  description = "The EC2 instance Private IP"
+  value       = aws_instance.main.private_ip
+}
